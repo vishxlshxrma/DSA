57568ffa56394f46adc914b2de2dd96cff7ec267
Vishal <vishalsharma3657@gmail.comgit>
2024-02-06 03:01:47 +0530

    Pattern1

---
+++ Patterns/Pattern1.py
@@ -0,0 +1,27 @@
+'''
+Given an integer N, print the following pattern.
+
+Example 1:
+Input: N = 3
+Output: 
+* * *
+* * *
+* * *
+
+Example 2:
+Input: N = 6
+Output:
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+'''
+
+def nForest(n: int) -> None:
+    for row in range(n):
+        for col in range(n):
+            print('* ', end="")
+        print('\n',end="")
