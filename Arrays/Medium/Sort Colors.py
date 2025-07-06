126179ec02d61585b351edb26fb043830a240928
Vishal <vishalsharma3657@gmail.comgit>
2025-07-05 22:08:08 -0700

    Sort Colors (Arrays Medium) Updated

--- Arrays/Medium/Sort Colors.py
+++ Arrays/Medium/Sort Colors.py
@@ -19,10 +19,35 @@ Output: [0,1,2]
 
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
-        n = len(nums)
-        for i in range(n-1,0,-1):
-            for j in range(i):
-                if (nums[j]>nums[j+1]):
-                    nums[j],nums[j+1] = nums[j+1],nums[j]
+        low, mid, high = 0, 0, len(nums) - 1
 
+        while (mid <= high):
+            if (nums[mid] == 0):
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low += 1
+                mid += 1
+            elif (nums[mid] == 1):
+                mid += 1
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high -= 1
         return nums
+    
+
+'''
+The Dutch-National Flag Algorithm 
+We initalize with 3 pointers and work towards sorting the array based on the pointers.
+Time Complexity - O(N)
+Space Complexity - O(1)
+
+We basically look for 3 possible cases for 3 values of mid here,
+
+Case 1 → Value of mid is 0
+Swap it with low because we want all 0's to be before mid.
+
+Case 2 → Value of mid is 1
+Increment mid because 1 is at the correct position it should be.
+
+Case 3 → Value of mid is 2
+We swap it with high because we want all 2's to be after high.
+'''
